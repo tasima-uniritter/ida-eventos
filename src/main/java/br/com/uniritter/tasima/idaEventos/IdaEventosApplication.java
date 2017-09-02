b5896f7fd1b99ec67f2a50d051506aad38938ac4
LucasGentile <lucasgpc@gmail.com>
2017-09-01 21:45:24 -0300

    Primeiro commit do projeto

---
+++ src/main/java/br/com/uniritter/tasima/idaEventos/IdaEventosApplication.java
@@ -0,0 +1,12 @@
+package br.com.uniritter.tasima.idaEventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdaEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdaEventosApplication.class, args);
+	}
+}
