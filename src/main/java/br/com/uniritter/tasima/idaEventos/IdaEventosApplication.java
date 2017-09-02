7ccc538f227e5ce30bcbff971a8620127e8466d9
LucasGentile <lucasgpc@gmail.com>
2017-09-02 08:57:25 -0300

    Cria branch development e corrige bug na dependência do lombok. Testa se o servidor roda

--- src/main/java/br/com/uniritter/tasima/idaEventos/IdaEventosApplication.java
+++ src/main/java/br/com/uniritter/tasima/idaEventos/IdaEventosApplication.java
@@ -1,17 +1,19 @@
 package br.com.uniritter.tasima.idaEventos;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
 @SpringBootApplication
+@Controller
 public class IdaEventosApplication {
-
+	@RequestMapping("/")
+	@ResponseBody
+	public String index(){
+		return "funciona?";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(IdaEventosApplication.class, args);
 	}
